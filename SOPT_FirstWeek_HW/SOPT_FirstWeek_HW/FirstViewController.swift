fb0a7a3bad361568d40e7bf067ed910dee4d7e11
qodhrkawk <gabriel1120@naver.com>
2020-04-24 10:40:00 +0900

    기존 프로젝트 파일 옮김

---
+++ SOPT_FirstWeek_HW/SOPT_FirstWeek_HW/FirstViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FirstViewController.swift
+//  SOPT_FirstWeek_HW
+//
+//  Created by Yunjae Kim on 2020/04/23.
+//  Copyright © 2020 Yunjae Kim. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ SOPT_FirstWeek_HW/SOPT_FirstWeek_HW/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  SOPT_FirstWeek_HW
+//
+//  Created by Yunjae Kim on 2020/04/18.
+//  Copyright © 2020 Yunjae Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    
+    
+
+
+}
+
